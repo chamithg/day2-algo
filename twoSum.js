ad3688974e82daac677f45fbc304f496669e551a
gamagegit <cgamage92@yahoo.com>
2022-09-01 11:16:54 -0700

    submit changes

--- twoSum.js
+++ twoSum.js
@@ -4,15 +4,17 @@
  * @return {number[]}
  */
 function twoSum(nums, target) {
-  for (i in nums) {
-    num1 = nums[i];
-    for (j = i; j < nums.length; j++) {
-      num2 = nums[j];
-      if (num1 + num2 == target) {
+  for (let i = 0; i < nums.length; i++) {
+    let num1 = nums[i]; //2
+
+    for (let j = i + 1; j < nums.length; j++) {
+      let num2 = nums[j]; //4
+
+      if (num1 + num2 === target) {
         return [i, j];
       }
     }
   }
 }
 
-console.log(twoSum([2, 7, 11, 15], 9));
+console.log(twoSum([3, 2, 4], 6));
